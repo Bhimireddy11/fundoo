e786b8fb0a5da188f12e5c330338d639121a601c
bridgelabz <rajibhimireddy@gmail.com>
2020-02-12 19:17:26 +0530

    [raji]api's,@annotations,controller added

--- src/main/java/com/bridgelabz/fundoonotes/Model/Note.java
+++ src/main/java/com/bridgelabz/fundoonotes/Model/Note.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.fundoonotes.Model;
 
 import java.time.LocalDateTime;
+
 import java.util.List;
 
 import javax.persistence.Column;
@@ -22,7 +23,6 @@ import lombok.Setter;
 
 @Entity
 @Data
-
 public class Note {
 	
 		@Id
@@ -44,121 +44,95 @@ public class Note {
 
 		@JsonIgnore
 		private LocalDateTime createdAt;
-
 		@JsonIgnore
 		private LocalDateTime updatedAt;
-
 		private String colour;
 
 		private LocalDateTime reminder;
-
 		@JsonIgnore
 		@ManyToOne
 		@JoinColumn(name = "userId")
 		private UserDemo userNotes;
-
 		@JsonIgnore
 		@ManyToMany
 		@JoinTable(name = "Label_Note", joinColumns = @JoinColumn(name = "note_id", referencedColumnName = "noteId"), inverseJoinColumns = @JoinColumn(name = "label_id", referencedColumnName = "labelId"))
 		private List<Label> labels;
-
-		public void setUserNotes(UserDemo userDemo) {
-			// TODO Auto-generated method stub
-			
-		}
-
 		public long getNoteId() {
 			return noteId;
 		}
-
 		public void setNoteId(long noteId) {
 			this.noteId = noteId;
 		}
-
 		public String getTitle() {
 			return title;
 		}
-
 		public void setTitle(String title) {
 			this.title = title;
 		}
-
 		public String getDescription() {
 			return description;
 		}
-
 		public void setDescription(String description) {
 			this.description = description;
 		}
-
 		public boolean isArchiev() {
 			return isArchiev;
 		}
-
 		public void setArchiev(boolean isArchiev) {
 			this.isArchiev = isArchiev;
 		}
-
 		public boolean isPin() {
 			return isPin;
 		}
-
 		public void setPin(boolean isPin) {
 			this.isPin = isPin;
 		}
-
 		public boolean isTrash() {
 			return isTrash;
 		}
-
 		public void setTrash(boolean isTrash) {
 			this.isTrash = isTrash;
 		}
-
 		public LocalDateTime getCreatedAt() {
 			return createdAt;
 		}
-
 		public void setCreatedAt(LocalDateTime createdAt) {
 			this.createdAt = createdAt;
 		}
-
 		public LocalDateTime getUpdatedAt() {
 			return updatedAt;
 		}
-
 		public void setUpdatedAt(LocalDateTime updatedAt) {
 			this.updatedAt = updatedAt;
 		}
-
 		public String getColour() {
 			return colour;
 		}
-
 		public void setColour(String colour) {
 			this.colour = colour;
 		}
-
 		public LocalDateTime getReminder() {
 			return reminder;
 		}
-
 		public void setReminder(LocalDateTime reminder) {
 			this.reminder = reminder;
 		}
-
-		public List<Label> getLabels() {
-			return labels;
-		}
-
-		public void setLabels(List<Label> labels) {
-			this.labels = labels;
-		}
-
 		public UserDemo getUserNotes() {
 			return userNotes;
 		}
-
+		public void setUserNotes(UserDemo userNotes) {
+			this.userNotes = userNotes;
+		}
+		public List<Label> getLabels() {
+			return labels;
+		}
+		public void setLabels(List<Label> labels) {
+			this.labels = labels;
+		}
+		@Override
+		public String toString() {
+			return "Note [noteId=" + noteId + ", title=" + title + ", desrition=" + description + ",  isArchiev=" +  isArchiev + ", isPin="
+					+ isPin + ", isTrash=" + isTrash + ", createdAt=" + createdAt + ", colour=" +colour
+					+ ", reminder=" + reminder + ",userNotes=" +userNotes +",Labels="+labels+"]";
+		}
 	}
-
-
